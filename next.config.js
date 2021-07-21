f20f23bb0bd0fa9ea9de86833c06fda9f0bd808d
Luke Davis <kule.visda@gmail.com>
2021-07-21 16:20:03 +0800

    Test anima export of figma page.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
-module.exports = withPlugins([
+module.exports = withPlugins(
   [
-    optimizedImages,
-    {
-      /* config for next-optimized-images */
-    },
+    [
+      optimizedImages,
+      {
+        /* config for next-optimized-images */
+      },
+    ],
+    // your other plugins here
   ],
-  // your other plugins here
   {
     reactStrictMode: true,
-  },
-]);
+  }
+);
